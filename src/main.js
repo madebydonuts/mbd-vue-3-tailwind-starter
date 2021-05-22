d10d156d9fe6311598b68c61d0fd0b649b4c8885
madebydonuts <83042214+madebydonuts@users.noreply.github.com>
2021-05-22 13:44:00 -0400

    added tailwind forms heroicons and headlessui deps

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import tailwind css utility framework
+import './assets/css/tailwind.css'
+
 createApp(App).use(store).use(router).mount('#app')
